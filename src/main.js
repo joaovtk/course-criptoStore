d3240c9930346d4b8c7ae43aa000421323fb200e
joaovtk <joaovitorsoaresdosreis3@gmail.com>
2023-12-03 11:57:59 -0300

    Deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import express, { Request, Response,  } from "express";
-import { engine } from "express-handlebars";
-import dotenv from "dotenv";
+const express = require("express");
+const { engine } = require("express-handlebars");
+const dotenv = require("dotenv");
 
 // set config
 let app = express();
@@ -10,21 +10,21 @@ app.set("views", __dirname+"/views");
 dotenv.config();
 app.use(express.static(__dirname+"/static"))
 
-app.get("/", (req: Request, res: Response): void => {
+app.get("/", (req, res) => {
     res.render("index", {layout: false})
 });
-app.get("/about", (req: Request, res: Response): void => {
+app.get("/about", (req, res) => {
     res.render("about", {layout: false})
 });
 
-app.get("/account", (req: Request, res: Response): void => {
+app.get("/account", (req, res)=> {
     res.render("account", {layout: false})
 });
 
-app.get("/buy", (req: Request, res: Response): void => {
+app.get("/buy", (req, res) => {
     res.render("temp", {layout: false});
 });
 
-app.listen(Number(process.env.PORT!), process.env.HOST!, (): void => {
+app.listen(Number(process.env.PORT), process.env.HOST, () => {
     console.log(`Listerner in http://${process.env.HOST}:${process.env.PORT}`)
 });

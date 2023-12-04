67fb65a8e0171d20b3bcca16f7e601cd36ad38a2
Proz <joaovitorsoaresdosreis3@gmail.com>
2023-12-04 15:01:21 -0300

    Update readme and resolving a errors

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Cripto Store 
-Um trabalho criado para o curso de dev sistemas da proz
+# Cripto Store - A cada dia mais perto do mercado de criptomoedas
+<p>O que é a cripto store: </p>
+<p>A cripto store é uma empresa em que vendemos nossas famosas cripto moedas, com ela podemos levar o mercado de cripto moedas para pessoas leigas com uma interface simples</p>
+
+# Homepage
+<p>A primeira parte que devemos atentar é que cada dia temos novas criptomoedas, e com isso podemos ficar atualizado e por isso que nossa home é adpatada para isso</p>
+
+# URL
+<p><a href="https://criptostore.onrender.com/">Cripto Store</a></p>
--- src/main.js
+++ src/main.js
@@ -8,17 +8,17 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname+"/views");
 dotenv.config();
-app.use(express.static(__dirname+"/static"))
+app.use(express.static(__dirname+"/static"));
 
 app.get("/", (req, res) => {
-    res.render("index", {layout: false})
+    res.render("index", {layout: false});
 });
 app.get("/about", (req, res) => {
-    res.render("about", {layout: false})
+    res.render("about", {layout: false});
 });
 
 app.get("/account", (req, res)=> {
-    res.render("account", {layout: false})
+    res.render("account", {layout: false});
 });
 
 app.get("/buy", (req, res) => {
@@ -28,3 +28,5 @@ app.get("/buy", (req, res) => {
 app.listen(Number(process.env.PORT), process.env.HOST, () => {
     console.log(`Listerner in http://${process.env.HOST}:${process.env.PORT}`)
 });
+
+module.exports = app;

4d5e477e8a190df83d0e04a21b9389056e948e84
Proz <joaovitorsoaresdosreis3@gmail.com>
2023-12-04 16:18:23 -0300

    Update the branch

--- src/main.js
+++ src/main.js
@@ -10,21 +10,6 @@ app.set("views", __dirname+"/views");
 dotenv.config();
 app.use(express.static(__dirname+"/static"));
 
-app.get("/", (req, res) => {
-    res.render("index", {layout: false});
-});
-app.get("/about", (req, res) => {
-    res.render("about", {layout: false});
-});
-
-app.get("/account", (req, res)=> {
-    res.render("account", {layout: false});
-});
-
-app.get("/buy", (req, res) => {
-    res.render("temp", {layout: false});
-});
-
 app.listen(Number(process.env.PORT), process.env.HOST, () => {
     console.log(`Listerner in http://${process.env.HOST}:${process.env.PORT}`)
 });

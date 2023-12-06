b69795e41b699be0e9c470f60771360c543be083
Proz <joaovitorsoaresdosreis3@gmail.com>
2023-12-06 15:41:18 -0300

    Finalizando por hoje

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const dotenv = require("dotenv");
-
+const pages = require("./routes/pages");
 // set config
 let app = express();
 app.engine("handlebars", engine());
@@ -9,39 +9,9 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname+"/views");
 dotenv.config();
 app.use(express.static(__dirname+"/static"));
+app.use(pages);
 
-app.get("/", (req, res) => {
-    res.render("index", {layout: false});
-});
-app.get("/about", (req, res) => {
-    res.render("about", {layout: false});
-});
-/*
-app.get("/account", (req, res) => {
-    res.render("account", {layout: false});
-});
-
-app.get("/account/register", (req, res) => {
-    
-});
-app.get("/account/login", (req, res) => {
-    
-});*/
-
-app.get("/login", (req, res) => {
-    res.render("login", {layout: false});
-});
-
-app.get("/test", (req, res) => {
-    res.render("login", {layout: false});
-});
-
-app.get("/buy", (req, res) => {
-    res.render("temp", {layout: false});
-});
-
+// set up the server
 app.listen(Number(process.env.PORT), process.env.HOST, () => {
     console.log(`Listerner in http://${process.env.HOST}:${process.env.PORT}`)
 });
-
-module.exports = app;

287324eb774de7ce3e7579784924a41a88a567e6
= <joaovitorsoaresdosreis3@gmail.com>
2024-04-08 15:19:37 -0300

    New route

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const dotenv = require("dotenv");
 const { MongoClient } = require("mongodb");
-
-
-
 // set config
 let app = express();
 app.engine("handlebars", engine());
@@ -40,18 +37,29 @@ app.get("/about", (req, res) => {
     res.render("about", {layout: false});
 });
 
-app.get("/account", (req, res)=> {
-    res.render("account", {layout: false});
+app.get("/create", (req, res)=> {
+    res.render("create", {layout: false});
+});
+app.get("/login", (req, res)=> {
+    res.render("login", {layout: false});
 });
 
 app.get("/buy", (req, res) => {
     res.render("temp", {layout: false});
 });
 
+app.post("/create", (req, res) => {
+    let user = req.query;
+    console.log(user);
+});
+app.get("/check", (req, res) => {
+    res.send("Teste").end();
+})
 
 app.listen(Number(process.env.PORT), process.env.HOST, () => {
     console.log(`Listerner in http://${process.env.HOST}:${process.env.PORT}`)
 });
+
 
 
 module.exports = app;
